5dd5bff3ae1ba8dbbad898aeff7e2b8a40df6d4f
gitee <rryyx2@outlook.com>
2025-10-25 23:57:38 +0800

    进行了修改，准备合并

--- main.py
+++ main.py
@@ -1,16 +1,9 @@
-# main.py
 
-# 导入你负责的乘除法函数
 from multiply_divide import multiply, divide
-
-# 为你的同学预留导入位置，他们实现功能后可以取消这行注释
-# from add_subtract import add, subtract
 
 print("欢迎使用简易计算器！")
 
-#获取用户表达式
 expression = input("请输入一个表达式 (中间请加空格,例如: 10 * 5): ")
-
 
 # "10 * 5" -> ["10", "*", "5"]
 parts = expression.split()
@@ -30,6 +23,5 @@ elif operator == '/':
 else:
     print(f"错误：不支持的运算符 '{operator}'")
 
-# 打印结果
 if result is not None:
     print(f"计算结果是: {result}")
