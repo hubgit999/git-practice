a57400e5a25cd3276a2135124bb9af919c935b24
Joshua-RFT <1580396681@qq.com>
2025-10-25 23:21:20 +0800

    完成

---
+++ main.py
@@ -0,0 +1,11 @@
+# main_app.py 文件
+
+# 从 calculator.py 文件中导入 simple_calculator 函数
+from add_subtract import simple_calculator
+
+print("开始调用计算器...")
+
+# 直接使用函数名调用它
+simple_calculator()
+
+print("计算器调用结束。")
